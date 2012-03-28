136edb8af3bfccd97601c8ae7f18a3ee3a532c45
Mark A. Matienzo <mark@matienzo.org>
2012-03-28 11:42:17 -0700

    Intitial import: Blacklight 3.3.0/blacklight-jetty 3.5.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bl::Application.config.secret_token = '0f8fffc15a5b9c75d91a098b81b446157eb100debbee338c0d36e641cfb88ed6a49a6b0ff76ad932a8fcb3ff70998cbaa4625f39b25340578f7843b2a8a752e2'
